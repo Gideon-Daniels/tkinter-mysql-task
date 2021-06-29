74aaac66ca5165692a9fe6ebf49dc4b7cadf8bda
Dragoonix <gideondaniels.dragoonix@gmail.com>
2021-06-29 14:21:14 +0200

    first commit

---
+++ connect_to_database.py
@@ -0,0 +1,48 @@
+class DatabaseHospital:
+    def __init__(self):
+        # Connecting to the database
+        # importing 'mysql.connector' as mysql for convenient
+        import mysql.connector as mysql
+        # connecting to the database using 'connect()' method
+        # it takes 3 required parameters 'host', 'user', 'passwd'
+        self.db = mysql.connect(
+            host="localhost",
+            user="lifechoices",
+            password="@Lifechoices1234",
+            database="Hospital"
+        )
+        # creating an instance of 'cursor' class which is used to execute the 'SQL' statements in 'Python'
+        self.cursor = self.db.cursor()
+
+    def select_username(self):
+        # defining the Query
+        query_user = "SELECT user FROM Login"
+        # getting records from the table
+        self.cursor.execute(query_user)
+        # fetching all records from the 'cursor' object
+        records = self.cursor.fetchall()
+        # showing the data
+        # for user in records:
+        #     return user
+        return records
+
+    def select_password(self):
+        query_password = "SELECT password FROM Login"
+        self.cursor.execute(query_password)
+        records = self.cursor.fetchall()
+        # for password in records:
+        #     return password
+        return records
+
+
+username = DatabaseHospital().select_username()
+password = DatabaseHospital().select_password()
+entered_username = "Gideon"
+print("USERNAMES :", username)
+print("PASSWORDS :", password)
+
+for x in username:
+    print(x)
+    if entered_username in x:
+        print("CORRECT")
+        break
---
+++ login.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from tkinter import messagebox
+from connect_to_database import DatabaseHospital
+
+
+class Login:
+    def __init__(self):
+        # creating window
+        self.window = Tk()
+        self.window.title("Login Page")
+        self.window.geometry("400x500")
+        self.window.config(bg="#323232")
+
+        # creating widgets
+        # Labels
+        self.label_username = Label(self.window, text="USERNAME",)
+        self.label_username.config(font="Arial 16 bold", bg="#323232", fg="#78cc6d")
+        self.label_username.place(y=100, x=50)
+        self.label_password = Label(self.window, text="PASSWORD",)
+        self.label_password.config(font="Arial 16 bold", bg="#323232", fg="#78cc6d")
+        self.label_password.place(y=150, x=50)
+        # Entries
+        self.entry_username = Entry(self.window,)
+        self.entry_username.config(highlightthickness=3, highlightbackground="#78cc6d", highlightcolor="#78cc6d")
+        self.entry_username.config(width=15, font="Arial 12 ", bg="#323232", fg="#78cc6d")
+        self.entry_username.place(y=100, x=200)
+        self.entry_password = Entry(self.window, )
+        self.entry_password.config(highlightthickness=3, highlightbackground="#78cc6d", highlightcolor="#78cc6d")
+        self.entry_password.config(width=15, font="Arial 12 ", bg="#323232", fg="#78cc6d")
+        self.entry_password.place(y=150, x=200)
+        # Buttons
+        self.button_login = Button(self.window, text="Login", command=self.login_button)
+        self.button_login.config(width=10, font="Arial 12 ", bg="#323232", fg="#78cc6d")
+        self.button_login.config(highlightthickness=1, highlightbackground="#78cc6d", highlightcolor="#78cc6d")
+        self.button_login.place(y=250, x=50)
+        self.button_login = Button(self.window, text="Register", command=self.register_button)
+        self.button_login.config(width=10, font="Arial 12 ", bg="#323232", fg="#78cc6d")
+        self.button_login.config(highlightthickness=1, highlightbackground="#78cc6d", highlightcolor="#78cc6d")
+        self.button_login.place(y=250, x=250)
+        self.button_login = Button(self.window, text="Exit", command=self.exiting)
+        self.button_login.config(width=10, font="Arial 12 ", bg="#323232", fg="#78cc6d")
+        self.button_login.config(highlightthickness=1, highlightbackground="#78cc6d", highlightcolor="#78cc6d")
+        self.button_login.place(y=300, x=150)
+        # repeatedly show window on screen
+        self.window.mainloop()
+
+    def login_button(self):
+        usernames = DatabaseHospital().select_username()
+        passwords = DatabaseHospital().select_password()
+        entered_username = self.entry_username.get()
+        entered_password = self.entry_password.get()
+        flag = False
+        for username in usernames:
+            if entered_username in username:
+                for password in passwords:
+                    if entered_password in password:
+                        messagebox.showinfo("WELCOME", "WELCOME")
+                        flag = True
+                        break
+        if not flag:
+            messagebox.showinfo("ERROR", "PLEASE ENTER VALID USERNAME AND PASSWORD")
+
+    def exiting(self):
+        pass
+
+    def register_button(self):
+        pass
+
+Login()
